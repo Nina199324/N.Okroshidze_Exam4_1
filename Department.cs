4ddf101e086946410b4a50be31de12ea4ecef2b2
Nino Okroshidze <114829776+Nina199324@users.noreply.github.com>
2022-11-30 22:47:35 +0400

    Add files via upload

---
+++ Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_6
+//6.2 შექმენით კლასი დეპარტამენტი რომელსაც ექნება დეპარტამენტის დასახელება და მაქსიმალური თანამშრომლის რაოდენობა (ველები).
+{
+    public class Department
+    {
+        public string departmentName;
+        public int NumofMaxEmpl { get; set; }
+
+        public string getDepartmentName()
+        {
+            return departmentName;
+        }
+
+        public int getEmpsMaxCount()
+        {
+            return NumofMaxEmpl;
+        }
+
+        public Department(string departName, int NumofMaxEmpl)
+        {
+            this.departmentName = departName;
+            this.NumofMaxEmpl = NumofMaxEmpl;
+        }
+    }
+}
+
+
---
+++ Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_6
+{
+    //6.1 შექმენით კლასი Employee რომელსაც ექნება სახელი, გვარი, პირადი ნომერი და ხელფასის ველები
+    class Employee
+    {
+        public string First_Name;
+        public string Last_Name;
+        public string ID_Number;
+        public double Salary_Field;
+
+        public Employee()
+
+        {
+            this.First_Name = First_Name;
+            this.Last_Name = Last_Name;
+            this.ID_Number = ID_Number;
+            this.Salary_Field = Salary_Field;
+
+            Console.Write("Enter Employee's First_Name :");
+            First_Name = Console.ReadLine();
+            Console.Write("Enter Employee's Last_Name :");
+            Last_Name = Console.ReadLine();
+            Console.Write("Enter ID_Number :");
+            ID_Number = Console.ReadLine();
+            Console.Write("Enter Salary_Field :");
+            Salary_Field = double.Parse(Console.ReadLine());
+
+
+        }
+        public void Print()
+        {
+            Console.Write("\n------------------------------");
+            Console.WriteLine("\nYou entered this data:");
+
+            Console.WriteLine("First Name :{0} \nLast Name :{1}\nId :{2}\nSalary_Field: {3} \n", First_Name, Last_Name, ID_Number, Salary_Field);
+        }
+    }
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Employee s1 = new Employee();
+            s1.Print();
+            Console.ReadLine();
+        }
+    }
+}
+
