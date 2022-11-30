93add26089a451da271b80982e632f1b741fc680
Nino Okroshidze <114829776+Nina199324@users.noreply.github.com>
2022-11-30 14:21:51 +0400

    Add files via upload

---
+++ Exam4_4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+//4.4 დაწერეთ პროგრამა რომელიც დააბრუნებს მასივში x-დან y შუალედში ელემენტებს.
+//x=10 y=20
+class Exam4_4
+{
+    static void Main()
+    {
+        int[] Matrice1 = {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19
+            };
+
+        Console.Write("\nLINQ : *We must find the numbers in the list with range after 10 till 20* ");
+        Console.Write(" \n The numbers in the array  are : \n");
+        Console.Write(" 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15 , 16, 17, 18, 19 \n");
+        var nQuery =
+        from VrNum in Matrice1
+        where VrNum > 10
+        where VrNum < 20          
+        select VrNum;
+        Console.Write("\nThe numbers with range after 10 till 20 are : \n");
+        foreach (var VrNum in nQuery)
+        {
+            Console.Write("{0}  ", VrNum);
+        }
+        Console.Write("\n\n");
+    }
+}
