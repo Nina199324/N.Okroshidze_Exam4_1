feb47fcf35cf05d2c1fa50c5984a997ef7ed0fcf
Nino Okroshidze <114829776+Nina199324@users.noreply.github.com>
2022-11-30 14:09:29 +0400

    Add files via upload

---
+++ Exam4_3.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+//4.3 დაწერეთ პროგრამა რომელიც სტრინგში (წინადადებაში იპოვნის uppercase-იან სიტყვბს და დაბეჭდავს მათ)
+
+
+class Exam4_3
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("\n Find the UPPERCASE words in a string : ");
+        Console.Write("\n----------------------------------------------\n");
+
+        string stringNew;
+
+        Console.Write("Input the string : ");
+        stringNew = Console.ReadLine();
+
+        var ucWord = WordFilt(stringNew);
+        Console.Write("\n < The UPPER CASE words are > :\n ");
+        foreach (string strRet in ucWord)
+        {
+            Console.WriteLine(strRet);
+        }
+        Console.ReadLine();
+    }
+
+    static IEnumerable<string> WordFilt(string mystr)
+    {
+        var wordforward = mystr.Split(' ')
+                    .Where(x => String.Equals(x, x.ToUpper(),
+                    StringComparison.Ordinal));
+
+        return wordforward;
+
+    }
+}
