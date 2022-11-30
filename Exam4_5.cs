3dab5ea43db24d7e42c4e7daa25efca45ede55e2
Nino Okroshidze <114829776+Nina199324@users.noreply.github.com>
2022-11-30 14:33:37 +0400

    Add files via upload

---
+++ Exam4_5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+//4.5   დაწერეთ პროგრამა რომელიც დაბეჭდავს სტრინგში ყველაზე განმეორებად სიმბოლოს.
+ 
+namespace LinQ
+{    
+    class Exam4_5
+    {
+        public static void Main()
+        {
+            string str = "I work for HeidelbergCement Georgia";
+            var mostFrequentCharacter = str.GroupBy(c => c).OrderByDescending(c => c.Count()).First().Key;
+            Console.Write("---------------------------------------------------------------------\n");
+            Console.WriteLine("Most Frequent Character is:");
+            Console.Write("---------------------------------------------------------------------\n");
+            Console.Write(mostFrequentCharacter);
+            
+        }
+    }
+}
+
